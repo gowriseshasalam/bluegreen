ef84b15cc530a2210e880a0794abcaa160865414
gowriseshasalam <42957234+gowriseshasalam@users.noreply.github.com>
2018-09-24 01:21:53 -1000

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,10 @@
+sudo apt-get install -y python-software-properties debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+echo "deb http://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install -y jenkins
